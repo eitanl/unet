b4e8d30cea0c2957dd461db11658cdaadb0efc39
Eitan <2028969+eitanl@users.noreply.github.com>
2025-05-19 10:49:11 +0300

    fix the Windows batch script

--- create_venv.bat
+++ create_venv.bat
@@ -1,3 +1,16 @@
-python -m venv env # or virtualenv env
-env\Scripts\activate
+@echo off
+echo Note: Run this script from Window's Command Prompt (not PowerShell)
+REM Window's PowerShell has its own activation script (Activate.ps1) and its own way calling it
+pause
+
+echo Installing virtual environment
+python -m venv env
+REM or virtualenv env
+
+echo Activating
+call env\Scripts\activate.bat
+
+echo Installing requirements
 pip install -r requirements.txt
+
+pip list
